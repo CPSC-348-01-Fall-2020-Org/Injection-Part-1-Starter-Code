8a0ed579b84e545cba63dc22f34b16b19b4160f5
Corwin Bryan <bryanc@gonzaga.edu>
2021-10-11 20:44:01 -0700

    Fixed some more stuff

--- gatherWithValidation.h
+++ gatherWithValidation.h
@@ -4,11 +4,13 @@ namespace injectionAttacks
 {
 	/**
 	 * A wrapper for the gather command that removes its vulnerabilities using blacklist validation.
+	 * Uses regexes and implements the principle of fail-safe defaults.
 	 */
     bool gatherWithBlacklist(std::string food, std::string storage);
 
 	/**
 	 * A wrapper for the gather command that removes its vulnerabilities using whitelist validation.
+	 * Uses regexes and implements the principle of fail-safe defaults.
 	 */
     bool gatherWithWhitelist(std::string food, std::string storage);
 }
